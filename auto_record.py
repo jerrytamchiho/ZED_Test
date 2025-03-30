3cd8b9963dc498cdd2fed494c0eac120a30c24cd
Jerry <jerrytamchiho@yahoo.com.hk>
2025-03-30 16:57:39 +0800

    Add: auto_record script

--- auto_record.py
+++ auto_record.py
@@ -23,6 +23,8 @@ import pyzed.sl as sl
 from signal import signal, SIGINT
 import argparse 
 import os 
+import time
+import datetime
 
 cam = sl.Camera()
 
@@ -42,12 +44,25 @@ def main():
     init.depth_mode = sl.DEPTH_MODE.ULTRA # Set configuration parameters for the ZED
     init.async_image_retrieval = True; # This parameter can be used to record SVO in camera FPS even if the grab loop is running at a lower FPS (due to compute for ex.)    
 
+    ts = time.time()
+    start_time = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
+    print('Starting time: ', start_time)
+
     status = cam.open(init) 
     if status != sl.ERROR_CODE.SUCCESS: 
         print("Camera Open", status, "Exit program.")
         exit(1)
+
+    # Get camera information (serial number)
+    zed_serial = cam.get_camera_information().serial_number
+    # print("Hello! This is my serial number: {}".format(zed_serial))
+
+
+    output_name = start_time + str(zed_serial) + '.svo2'
+    print('Output file: ', output_name)
+    output_path = os.path.join('/home/vizlab/Desktop/recordings/', output_name)
         
-    recording_param = sl.RecordingParameters(opt.output_svo_file, sl.SVO_COMPRESSION_MODE.H264) # Enable recording with the filename specified in argument
+    recording_param = sl.RecordingParameters(output_path, sl.SVO_COMPRESSION_MODE.H264) # Enable recording with the filename specified in argument
     err = cam.enable_recording(recording_param)
     if err != sl.ERROR_CODE.SUCCESS:
         print("Recording ZED : ", err)
@@ -56,39 +71,30 @@ def main():
     runtime = sl.RuntimeParameters()
     print("SVO is Recording, use Ctrl-C to stop.") # Start recording SVO, stop with Ctrl-C command
     frames_recorded = 0
+    frames_dropped = 0
+
+    image = sl.Mat()
 
     while True:
         if cam.grab(runtime) == sl.ERROR_CODE.SUCCESS : # Check that a new image is successfully acquired
+            cam.retrieve_image(image, sl.VIEW.LEFT)
             frames_recorded += 1
-            # print("Frame count: " + str(frames_recorded), end="\r")
+            print("Frame count: " + str(frames_recorded), end="\r")
+            print("Image resolution: {0} x {1}\n".format(image.get_width(), image.get_height()))
             # print(str(cam.get_timestamp(sl.TIME_REFERENCE.IMAGE).get_microseconds()))
+        else:
+            frames_dropped += 1
 
-    print(opt.output_svo_file)
-
+    print(f'Frames dropped: {frames_dropped}')
     cam.disable_recording()
     cam.close()
     sys.exit(0)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output_svo_file', type=str, help='Path to the SVO file that will be written', required=False, default='output.svo2')
-    parser.add_argument('--timestamp', type=str, help='Starting timestamp from host')
     opt = parser.parse_args()
-    if not opt.output_svo_file.endswith(".svo") and not opt.output_svo_file.endswith(".svo2"): 
-        print("--output_svo_file parameter should be a .svo file but is not : ", opt.output_svo_file,"Exit program.")
-        exit()
-
-    # if len(sys.argv) > 1:
-    #     timestamp = sys.argv[1]
-    #     print(f'timestamp: {timestamp}')
-    # else:
-    #     print('No timestamp provided')
-
-    try:
-        received_time = opt.timestamp
-        print(received_time)
-        print('-------------------------------')
-    except ValueError:
-        print("No received time")
+    # if not opt.output_svo_file.endswith(".svo") and not opt.output_svo_file.endswith(".svo2"): 
+    #     print("--output_svo_file parameter should be a .svo file but is not : ", opt.output_svo_file,"Exit program.")
+    #     exit()
 
     main()
