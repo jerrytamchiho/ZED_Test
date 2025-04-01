e3b1230396b6e0834dfefb732ff3597b3b87f058
Jerry <jerrytamchiho@yahoo.com.hk>
2025-04-01 11:02:05 +0800

    Handle disconnection. Display video feed

--- auto_record.py
+++ auto_record.py
@@ -25,6 +25,7 @@ import argparse
 import os 
 import time
 import datetime
+import cv2
 
 cam = sl.Camera()
 
@@ -32,6 +33,7 @@ cam = sl.Camera()
 def handler(signal_received, frame):
     cam.disable_recording()
     cam.close()
+    cv2.destroyAllWindows() 
     sys.exit(0)
 
 signal(SIGINT, handler)
@@ -57,11 +59,15 @@ def main():
     zed_serial = cam.get_camera_information().serial_number
     # print("Hello! This is my serial number: {}".format(zed_serial))
 
-
+    output_dir = '/home/vizlab/Desktop/recordings'
+    # output_dir = '~/Desktop/recordings'
     output_name = start_time + str(zed_serial) + '.svo2'
     print('Output file: ', output_name)
-    output_path = os.path.join('/home/vizlab/Desktop/recordings/', output_name)
-        
+    output_path = os.path.join(output_dir, output_name)
+
+    os.makedirs(output_dir, exist_ok=True)
+    print(output_path)
+
     recording_param = sl.RecordingParameters(output_path, sl.SVO_COMPRESSION_MODE.H264) # Enable recording with the filename specified in argument
     err = cam.enable_recording(recording_param)
     if err != sl.ERROR_CODE.SUCCESS:
@@ -71,23 +77,33 @@ def main():
     runtime = sl.RuntimeParameters()
     print("SVO is Recording, use Ctrl-C to stop.") # Start recording SVO, stop with Ctrl-C command
     frames_recorded = 0
-    frames_dropped = 0
 
     image = sl.Mat()
 
-    while True:
-        if cam.grab(runtime) == sl.ERROR_CODE.SUCCESS : # Check that a new image is successfully acquired
-            cam.retrieve_image(image, sl.VIEW.LEFT)
-            frames_recorded += 1
-            print("Frame count: " + str(frames_recorded), end="\r")
-            print("Image resolution: {0} x {1}\n".format(image.get_width(), image.get_height()))
-            # print(str(cam.get_timestamp(sl.TIME_REFERENCE.IMAGE).get_microseconds()))
-        else:
-            frames_dropped += 1
+    try:
+        while True:
+            status = cam.grab(runtime)
+            if status == sl.ERROR_CODE.SUCCESS : # Check that a new image is successfully acquired
+                cam.retrieve_image(image, sl.VIEW.LEFT, sl.MEM.CPU, sl.Resolution(800, 600))
+                frames_recorded += 1
+                print("Frame count: " + str(frames_recorded), end="\r")
+                img_cv = image.get_data()  # Get image data in numpy array format
+                cv2.imshow("ZED Camera Feed", img_cv)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        print(f'Close recording')
+        cam.disable_recording()
+        cam.close()
+        cv2.destroyAllWindows() 
 
-    print(f'Frames dropped: {frames_dropped}')
     cam.disable_recording()
     cam.close()
+    cv2.destroyAllWindows() 
     sys.exit(0)
     
 if __name__ == "__main__":
