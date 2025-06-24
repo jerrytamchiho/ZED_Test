bf146f849f92c142e288466d091865409b07e265
Jerry <jerrytamchiho@yahoo.com.hk>
2025-06-24 18:14:10 +0800

    typo

--- fusion_sender.py
+++ fusion_sender.py
@@ -79,7 +79,7 @@ def main():
     body_runtime_param.skeleton_smoothing = 0.7
 
     communication_param = sl.CommunicationParameters()
-    communication_parameters.setForLocalNetwork(30003, "192.168.0.135")
+    communication_param.setForLocalNetwork(30003, "192.168.0.135")
     zed.start_publishing(communication_param)
     print("Start publishing")
 
